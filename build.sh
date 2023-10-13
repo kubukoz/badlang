513bfbddca6774ddbcf74c09a528910a17f9ab6a
Jakub Kozłowski <kubukoz@gmail.com>
2023-10-14 00:38:59 +0200

    cleanup scripts

--- build.sh
+++ build.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if [ -z "$NATIVE" ]
-then
-  echo "Building jvm"
-  scala-cli package . -o badlang-lsp --main-class badlang.main -f $@
-else
-  echo "Building native"
-  scala-cli package . -o badlang-lsp --main-class badlang.main -f --native $@
-fi
+scala-cli package . -o badlang-lsp --main-class badlang.main -f $@
